9c850eba62b9da6153046cbd36b544930a0ac3b9
Vandijstadt Nicolas <81445233+vandijstadt@users.noreply.github.com>
2025-01-04 18:53:42 +0100

    Delete start creation

--- app/src/main/java/be/school/quizzapplication/fragments/GestionFragment.kt
+++ app/src/main/java/be/school/quizzapplication/fragments/GestionFragment.kt
@@ -1,14 +1,23 @@
 package be.school.quizzapplication.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import be.school.quizzapplication.DTO.quizz.GetAllQuizzesResponse
 import be.school.quizzapplication.R
 import be.school.quizzapplication.databinding.FragmentGestionBinding
+import be.school.quizzapplication.repository.IQuizzRepository
+import com.school.tmproject.placeholder.RetrofitFactory
+import kotlinx.coroutines.launch
+import java.net.CookieHandler
+import java.net.CookieManager
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,15 +69,56 @@ class GestionFragment : Fragment() {
         super.onDestroyView()
         binding = null // Release binding to prevent memory leaks
     }
+
+    private fun setUpToken() {
+        val cookieManager = CookieManager()
+        CookieHandler.setDefault(cookieManager)
+
+        val cookies = cookieManager.cookieStore.cookies
+        for(cookie in cookies){
+            if(cookie.name == "token"){
+                val token = cookie.value
+                RetrofitFactory.addTokenToClient(token)
+            }
+        }
+    }
+
     private fun setUpListener() {
         binding?.buttonDelete?.setOnClickListener {
             Log.i("Nicolas delete", "Debut de la suppression")
-
-
-
+            AlertDialog.Builder(context)
+                .setTitle("Confirm Deletion")
+                .setMessage("Are you sure you want to delete this quiz?")
+                .setPositiveButton("Delete") { dialog, which ->
+                    performDelete(1) // TODO : a changer
+                }
+                .setNegativeButton("Cancel") { dialog, which ->
+                }
+                .show()
         }
         binding?.buttonModify?.setOnClickListener {
             Log.i("Nicolas update", "Debut de la modification")
+        }
+    }
+    private val apiService = RetrofitFactory.instance.create(IQuizzRepository::class.java)
+    private fun performDelete(quizzId: Int) {
+        lifecycleScope.launch {
+            try {
+                val response: Boolean = apiService.delete(quizzId)
+                if (response) {
+                    Log.d("Nicolas delete", "Delete successful")
+                    // TODO : messages
+                    Toast.makeText(context, "Successful removal", Toast.LENGTH_LONG)
+                } else {
+                    // TODO : messages alerte
+                    Toast.makeText(context, "Failed removal", Toast.LENGTH_LONG)
+//                    Log.e("Nicolas Login", "Login failed: ${response.code()} ${response.message()}")
+                }
+            } catch (e: Exception) {
+                Toast.makeText(context, "Failed removal", Toast.LENGTH_LONG)
+                Log.e("Nicolas delete", "Login error: ${e.message}")
+            }
+            Log.i("Nicolas delete", "Out login...")
         }
     }
 }
