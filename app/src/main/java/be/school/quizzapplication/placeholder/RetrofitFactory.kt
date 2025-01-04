28bc7187add7617e1ecd3108cefadafb769d51e7
NextAndCie <117162264+NextAndCie@users.noreply.github.com>
2025-01-04 17:42:25 +0100

    Updated RetrofitFactory and LoginActivity, to add interceptor to requests and allow LoginActivity to update RetrofitFactory with the token created from login in.

--- app/src/main/java/be/school/quizzapplication/placeholder/RetrofitFactory.kt
+++ app/src/main/java/be/school/quizzapplication/placeholder/RetrofitFactory.kt
@@ -1,16 +1,34 @@
 package com.school.tmproject.placeholder
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitFactory {
-        private const val URL:String = "http://10.0.2.2:5094/v1/" // Intern a android studio
+    private const val URL:String = "http://10.0.2.2:5094/v1/" // Intern a android studio
 //    private const val URL: String = "http://192.168.1.5:5094/v1/" // Secondaire
+    private val client = OkHttpClient.Builder().build()
 
-    val instance: Retrofit = Retrofit.Builder()
+    val instance: Retrofit
+        get() = Retrofit.Builder()
         .baseUrl(URL)
+        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
+    fun addTokenToClient(token: String){
+        val interceptor = Interceptor { chain ->
+            val newRequest = chain.request().newBuilder()
+                .addHeader("Authorization", "Bearer ${token}")
+                .build()
+            chain.proceed(newRequest)
+        }
 
+        val newClient = client.newBuilder()
+            .addInterceptor(interceptor)
+            .build()
+
+        instance.newBuilder().client(newClient).build()
+    }
 }
