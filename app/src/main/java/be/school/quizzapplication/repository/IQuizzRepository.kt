bdfe08559d4b3858db7e752baf023a4049ed7ec0
Vandijstadt Nicolas <81445233+vandijstadt@users.noreply.github.com>
2025-01-04 14:51:46 +0100

    Hold click

--- app/src/main/java/be/school/quizzapplication/repository/IQuizzRepository.kt
+++ app/src/main/java/be/school/quizzapplication/repository/IQuizzRepository.kt
@@ -1,6 +1,6 @@
 package be.school.quizzapplication.repository
 
-import be.school.quizzapplication.dto.quizz.GetAllQuizzesResponse
+import be.school.quizzapplication.DTO.quizz.GetAllQuizzesResponse
 import retrofit2.http.GET
 
 interface IQuizzRepository {
