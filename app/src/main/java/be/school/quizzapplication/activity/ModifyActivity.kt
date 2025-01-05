67fb761adf1a7c2ff4bfb0f093f32c8540692240
Vandijstadt Nicolas <81445233+vandijstadt@users.noreply.github.com>
2025-01-05 19:04:56 +0100

    On recupere bien le quizz

--- app/src/main/java/be/school/quizzapplication/activity/ModifyActivity.kt
+++ app/src/main/java/be/school/quizzapplication/activity/ModifyActivity.kt
@@ -3,13 +3,9 @@ package be.school.quizzapplication.activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import be.school.quizzapplication.DTO.quizz.GetAllQuizzesResponse
-import be.school.quizzapplication.R
 import be.school.quizzapplication.databinding.ActivityModifyBinding
 import be.school.quizzapplication.repository.IQuizzRepository
 import com.school.tmproject.placeholder.RetrofitFactory
@@ -17,30 +13,32 @@ import kotlinx.coroutines.launch
 
 class ModifyActivity : AppCompatActivity() {
     private lateinit var binding: ActivityModifyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_modify)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityModifyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val quizId = intent.getIntExtra("id", -1)
         performGetQuiz(quizId)
     }
 
+
     private val apiService = RetrofitFactory.instance.create(IQuizzRepository::class.java)
-    private fun performGetQuiz(id: Int) {
+    private fun performGetQuiz(quizId: Int) {
         val intent = Intent(this, QuizzManagerActivity::class.java)
         lifecycleScope.launch {
             try {
-                val response: GetAllQuizzesResponse = apiService.getById(id)
-//                binding.editTextId.text.append(response.id.toString())
+                Log.i("Nicolas quiz", "Id : $quizId")
+                val response: GetAllQuizzesResponse = apiService.getById(quizId)
+                Log.i("Nicolas quiz", response.toString())
+                binding.editTextId.text.append(response.id.toString())
+                binding.editTextTitle.text.append(response.title)
             } catch (e: Exception) {
-                Log.e("Nicolas Login", "Login error: ${e.message}")
+                Log.e("Nicolas quiz", "quiz error: ${e.message}")
             }
-            Log.i("Nicolas Login", "Out login...")
+            Log.i("Nicolas quiz", "Out quiz...")
         }
     }
 }
